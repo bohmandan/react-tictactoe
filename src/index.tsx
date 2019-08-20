2e3b1730139799d995d38eaeedc7686fb44af11e
bohmandan <bohmandan@gmail.com>
2019-08-20 22:17:00 +0200

    change folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Game } from './Game';
+import { Game } from './components/Game';
 import './style.css';
 
 // Continue with tutorial here:
